3476b2d21a2efcbe97ec67e571994cafbcf47a85
MEUNIER Sébastien <stg3005@brufor.lan>
2024-04-26 08:53:04 +0200

    confine mouse to window in menu

--- Assets/#Project/Scripts/UnlockCursor.cs
+++ Assets/#Project/Scripts/UnlockCursor.cs
@@ -7,7 +7,7 @@ public class UnlockCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
     }
 
